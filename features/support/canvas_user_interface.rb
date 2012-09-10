da71a8c47e70daa32d06dccee428214cbebbaa96
praqriti <praqriti.singh@gmail.com>
2012-09-10 14:24:04 +0530

    added config to run tests on test environment

--- features/support/canvas_user_interface.rb
+++ features/support/canvas_user_interface.rb
@@ -4,27 +4,30 @@ require 'rubygems'
 
 module CanvasUserInterface
 
-  @account_id = 1
+  @account_id = 2
+  @user_exists = false
   
   def self.create_user(login_id,password)
-    
-  delete_user(login_id)
-  
+  delete_user(login_id)  
   
   @last_response =
   JSONSpecInterface.post("#{CANVAS_API}/accounts/#{@account_id}/users",
-  :body => {  "user[name]" => "username",
-              "pseudonym[unique_id]" => "#{login_id}",
-              "pseudonym[password]" => "#{password}"                
-              },
-  :headers => { "Authorization" => "#{CANVAS_ACCESS_TOKEN}"})
+  :body =>  {:pseudonym =>
+            {
+              :unique_id => "#{login_id}",
+              :password => "#{password}"
+            }
+            },
+  :headers => { "Authorization" => "#{CANVAS_ACCESS_TOKEN}"}).to_json
 end
 
 
 def self.delete_user(login_id)
   user = CanvasUserInterface.find_user(login_id)
+  if(@user_exists)
         JSONSpecInterface.delete("#{CANVAS_API}/accounts/#{@account_id}/users/#{user["id"]}",
         :headers => { "Authorization" => "#{CANVAS_ACCESS_TOKEN}"})
+  end
 end
 
 def self.find_user(login_id)
@@ -32,6 +35,7 @@ def self.find_user(login_id)
      :headers => { "Authorization" => "#{CANVAS_ACCESS_TOKEN}"})
      @users_list.parsed_response.each do |user|
         if(user["login_id"]==login_id)
+          @user_exists =true
             return user
         end  
       end  
