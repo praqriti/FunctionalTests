6d636961d08d66a9b2e52e2522adcb3ba2f89abb
ans7991 <anshul9411224410@gmail.com>
2012-10-12 18:19:02 +0530

    Web my wall f.tests

--- features/step_definitions/web_steps/web_my_wall_steps.rb
+++ features/step_definitions/web_steps/web_my_wall_steps.rb
@@ -1,7 +1,6 @@
 Given /^User lands on My Wall and can view all the elements$/ do
-   @app.my_wall = MyWall.new
-   @app.my_wall.wait_until_user_name_visible
-   @app.my_wall.all_there?
+  my_wall = MyWall.new
+  my_wall.wait_until_user_name_visible
 end
 
 Given /^User clicks on My Wall$/ do
@@ -12,47 +11,47 @@ Given /^User clicks on My Wall$/ do
 end
 
 Then /^User can view her latest status message "(.*?)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+  @app.my_wall.status_message.text.should == arg1
 end
 
 Then /^User can view the comments attached to the status message$/ do
-  pending # express the regexp above with the code you wish you had
+  @app.my_wall.wait_until_comment_text_visible
+  @app.my_wall.comment_text.should_be_displayed
 end
 
-Then /^User can view the information of the user who has commented on the status message$/ do
-  pending # express the regexp above with the code you wish you had
+Then /^User can view the username of the user who has commented on the status message$/ do
+  @app.my_wall.commented_by.should_be_displayed
 end
 
 Then /^User comments "(.*?)" on her status message$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+  @app.my_wall.wait_until_comment_box_visible
+  @app.my_wall.comment_box.set "#{arg1}"
+  @app.my_wall.comment_submit.click
 end
 
-Then /^the comment is visible on My Wall$/ do
-  pending # express the regexp above with the code you wish you had
+Then /^the comment "(.*?)" is visible on My Wall$/ do |arg1|
+  @app.my_wall.comment_text.text.should == arg1
 end
 
-Then /^User can view her latest status message$/ do
-  pending # express the regexp above with the code you wish you had
+Then /error message "(.*?)" is displayed$/ do |error1|
+  @app.my_wall.error_message.text.should == error1
 end
 
 Then /^User cannot enter blank comment on the status$/ do
-  pending # express the regexp above with the code you wish you had
+  @app.my_wall.comment_box.set ""
+  @app.my_wall.comment_submit.click
+  steps %{
+    Then error message "Can not post an empty comment. Please re-enter comment before posting" is displayed
+  }
 end
 
 Then /^User comments on her status message:$/ do |string|
-  pending # express the regexp above with the code you wish you had
+  @app.my_wall.comment_box.set "#{string}"
+  @app.my_wall.comment_submit.click
 end
 
 Then /^the comment visible is:$/ do |string|
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^the comment "(.*?)" is visible on top of My Wall$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^User must be able to navigate to the wall of  "(.*?)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+  @app.my_wall.comment_text.text.should == string
 end
 
 
