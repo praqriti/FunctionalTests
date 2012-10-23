8ca36eec0695ec99eac93f06fc68a748005e5bf1
ans7991 <anshul9411224410@gmail.com>
2012-10-23 18:00:08 +0530

    added more scenarios to course lists

--- features/USSD_features/ussd_courses_list.feature
+++ features/USSD_features/ussd_courses_list.feature
@@ -10,15 +10,15 @@ Background:
 	Given I make a new USSD login request
 	When User "camfed_student" logs into USSD with her credentials
 	Then "camfed_student" should see the USSD home page
-
-Scenario: View Courses list
-	
 	Given the following courses exist in canvas
 	|COURSE|
 	|History|
 	|Chemistry|
 	|Mathematics|
 	|Computer|
+
+Scenario: View Courses list
+	
 	And User is enrolled to the following courses
 	|COURSE|
 	|History|
@@ -26,10 +26,21 @@ Scenario: View Courses list
 	Given User chooses the option "Courses"
 	Then User should see the courses list
 	Then the created courses are deleted
-@wip	
+	
 Scenario: Verify back from courses page from ussd
 	
+	And User is enrolled to the following courses
+	|COURSE|
+	|History|
+	|Chemistry|
 	Given User chooses the option "Courses"
 	And User should see the courses list
 	When User replies "0" from courses page to go back to home page
 	Then "camfed_student" should see the USSD home page
+	Then the created courses are deleted
+
+Scenario: User is not enrolled to any course
+
+	Given User chooses the option "Courses"
+	Then "No Course Attached" should be displayed to the User
+	Then the created courses are deleted
--- features/step_definitions/USSD_steps/USSD_courses_list_steps.rb
+++ features/step_definitions/USSD_steps/USSD_courses_list_steps.rb
@@ -55,20 +55,15 @@ end
 
 Then /^User should see the courses list$/ do
 	user_id = CanvasUserInterface.get_user_id
-	@enrolled_courses.reverse! { |c1, c2| c1.name <=> c2.name }
-
-  actual_response = @last_response.parsed_response
-
-  s_no = 1
-
-  @enrolled_courses.each do |enrolled_course|
-    actual_response["response_map"]["#{s_no}"]["text"].should == enrolled_course.name
-    actual_response["response_map"]["#{s_no}"]["url"].should  == "/sen/users/#{user_id}/courses/#{enrolled_course.id}/quizzes"
-    s_no+=1
-  end
-  actual_response["response_map"]["0"]["text"].should == "Home"
-  actual_response["response_map"]["0"]["url"].should  == "sen/users/#{user_id}"
-
+	actual_response = @last_response.parsed_response
+	s_no = 1
+	@enrolled_courses.each do |enrolled_course|
+		actual_response["response_map"]["#{s_no}"]["text"].should == enrolled_course.name
+		actual_response["response_map"]["#{s_no}"]["url"].should  == "/sen/users/#{user_id}/courses/#{enrolled_course.id}/quizzes"
+		s_no+=1
+  	end
+	actual_response["response_map"]["0"]["text"].should == "Home"
+	actual_response["response_map"]["0"]["url"].should  == "sen/users/#{user_id}"
 	steps %{
 		Then the JSON at "session_id" should be "session id"
 		Then the JSON at "session_type" should be "SESSION"
@@ -78,30 +73,30 @@ end
 
 When /^User replies "0" from courses page to go back to home page$/ do
 	user_id = CanvasUserInterface.get_user_id
+	body = @last_response.parsed_response.merge!({"message" => "0"})
 	@last_response = JSONSpecInterface.post("#{SEN_URL}",
-      	:body => {
-		  :session_id => "session id",
-		  :session_type => "SESSION",
-		  :message => "0",
-		  :access_token =>"#{@last_response.parsed_response["access_token"]}",
-		  :response_map =>
-		  {				
-				"2"=> {
-				"text"=>"History",
-				"url"=>""
-				},
-				"1"=> {
-				"text"=>"Chemistry",
-				"url"=>""
-				},
-				"0"=> {
-				"text"=>"Home",
-				"url"=>"sen/users/#{user_id}"
-				}
-		  }
-	}.to_json,
+       	:body =>body.to_json,
 	:headers => { "Content-Type" => "application/json"})
 end
+
+Then /^"([^\"]*)" should be displayed to the User$/ do |message|
+	user_id = CanvasUserInterface.get_user_id
+	steps %{
+		Then the JSON at "message" should be "#{message}"
+		Then the JSON at "session_id" should be "session id"
+		Then the JSON at "session_type" should be "SESSION"
+		Then the JSON should have "access_token"
+		Then the JSON at "response_map" should be:
+		"""
+		{
+			"0": {
+			"text": "Home",
+			"url": "sen/users/#{user_id}"
+			}
+		}
+		"""
+    		}
+end	
 
 Then /^the created courses are deleted$/ do
 	@courses.each do |course|      
