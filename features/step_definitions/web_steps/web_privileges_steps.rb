f53e13fcc0c94a4174d5ee36507bc989569799cc
praqriti <praqriti.singh@gmail.com>
2012-11-09 12:47:23 +0530

    web test in progress

---
+++ features/step_definitions/web_steps/web_privileges_steps.rb
@@ -0,0 +1,23 @@
+When /^User tries to update status for "(.*?)" as "(.*?)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^User tries to view "(.*?)" for user "(.*?)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^User should be able to see only his connection "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^"(.*?)" tries to view posts for user "(.*?)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^User is given an authorisation error$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^"(.*?)" tries to view comments for user "(.*?)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
