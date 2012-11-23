e25710d1f6fa2c707dff6ddae09b3451fe9731ca
Anuj Jamwal <anuj.jamwal1@gmail.com>
2012-11-23 16:13:56 +0530

    #340 [Anuj] fixing the tests to pick the correct element

--- features/web_pages/sections/web_connection_details_section.rb
+++ features/web_pages/sections/web_connection_details_section.rb
@@ -1,7 +1,7 @@
 class ConnectionDetailsSection < SitePrism::Section
   
-  element :accept_button, ".connection_action a.accept"
-  element :reject_button, ".connection_action a.reject" 
+  element :accept_button, ".connection_action .accept input.social-button"
+  element :reject_button, ".connection_action .reject input.social-button"
   element :connection_message, ".connection_action div.message"
   element :username, ".title"
   element :user_image, "#search_result div.user img"
--- features/web_pages/web_pending_connection_page.rb
+++ features/web_pages/web_pending_connection_page.rb
@@ -3,8 +3,8 @@ class ConnectionRequests < SitePrism::Page
   
   element :header_message, ".sen_header"
   element :user, ".user"
-  element :accept_button, ".connection_action a.accept"
-  element :reject_button, ".connection_action a.reject" 
+  element :accept_button, ".connection_action .accept input.social-button"
+  element :reject_button, ".connection_action .reject input.social-button"
   element :connection_message, ".connection_action div.message"
   element :username, ".user .title"
   element :connection_alert, ".notice .message" 
