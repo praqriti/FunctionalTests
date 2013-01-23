469d8c598386b27228efc54be16820cd39e1d898
praqriti <praqriti.singh@gmail.com>
2013-01-23 15:03:45 +0530

    fixing retries in FTs

--- features/support/env.rb
+++ features/support/env.rb
@@ -19,7 +19,7 @@ end
 
 def retry_on_timeout(n = 3, &block)
   block.call
-rescue Capybara::TimeoutError, Capybara::ElementNotFound => e
+rescue Capybara::ElementNotFound => e
   if n > 0
     puts "Catched error: #{e.message}. #{n-1} more attempts."
     retry_on_timeout(n - 1, &block)
